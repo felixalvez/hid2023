026b2bc2beab9a77966c94a799f0597c51489b2e
Felix Alvez <felixalvez@gmail.com>
2023-05-11 23:59:32 -0500

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const people = require("./people.json");
+
 const app = express();
 
-const server = app.listen(8080, () => {
-  console.log(`Express running -> PORT ${server.address().port}`);
-});
+app.set("view engine", "pug");
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index", {
+    title: "Homepage",
+    people: people.profiles,
+  });
+});
+
+app.get("/profile", (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render("profile", {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
+const server = app.listen(8080, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
 });
