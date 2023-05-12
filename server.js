03bb58b8b02999af764215086628a3f3bb8d42f7
Felix Alvez <felixalvez@gmail.com>
2023-05-12 01:52:18 -0500

    first commit

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ app.set("view engine", "pug");
 
 app.use(express.static(__dirname + "/public"));
 
+/* #1
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+*/
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Homepage",
+    // name: "giannis",
+  });
+});
+
+/* Step 3
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Homepage",
@@ -14,6 +28,24 @@ app.get("/", (req, res) => {
   });
 });
 
+<!--
+extends default
+
+block content
+  div.container
+    each person in people
+      div.person
+        div.person-image(style=`background: url('/images/${person.imgSrc}') top center
+        no-repeat; background-size: cover;`)
+        h2.person-name
+          | #{person.firstname} #{person.lastname}
+        a(href=`/profile?id=${person.id}`)
+          | View Profile
+-->
+
+*/
+
+/* Step 4
 app.get("/profile", (req, res) => {
   const person = people.profiles.find((p) => p.id === req.query.id);
   res.render("profile", {
@@ -21,6 +53,8 @@ app.get("/profile", (req, res) => {
     person,
   });
 });
+
+*/
 const server = app.listen(8080, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
